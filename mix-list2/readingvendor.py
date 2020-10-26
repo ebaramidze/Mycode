fc91916a4b2bcf12abe01cf2f8df80b4232412eb
Eugene <forbarik@gmail.com>
2020-10-26 22:00:10 +0000

    Looping in CVS file

---
+++ mix-list2/readingvendor.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""using csv data"""
+import csv
+
+def main():
+    with open("vendor.csv", "r") as vendorfile:
+        vendata = csv.reader(vendorfile, delimiter=",")
+        for row in vendata:
+            print(f"The IP address {row[2]} requires the driver {row[3]}" )
+
+main()
+
