03ef11568b836dd9e6ed91d44519214d0e179f37
Eugene <forbarik@gmail.com>
2020-10-28 13:31:43 +0000

    Day two second half

---
+++ Challenge1/challenge1.py
@@ -0,0 +1,13 @@
+dogs = []
+fish = []
+
+pets = [{"type": "dog", "name": "mudge", "age": 4},
+        {"type": "fish", "name": "dorothy", "age": "3 days"},
+        {"type": "dog", "name": "bingo", "age": 12},
+        {"type": "fish", "name": "nemo", "age": 2},
+        {"type": "cat", "name": "fluffy", "age": 1}]
+
+for pet in pets:
+    print(pet)
+#print(pet["type"])
+
